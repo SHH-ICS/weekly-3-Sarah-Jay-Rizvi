30f6692ca33302ae7aa406db4c7a9184ec47bcca
Sarah-Jay-Rizvi <sarah.rizvi@stu.ocsb.ca>
2024-10-25 14:50:17 +0000

    Still working on it.

--- assignment.py
+++ assignment.py
@@ -1,1 +1,18 @@
+import math
 
+(input("Enter your Order Below."))
+
+s = size 
+l = float(6.00) 
+x = float(10.00)
+
+c = cost 
+t = float(1.00)
+t2 = float(1.75)
+t3 = float(2.50)
+t4 = float(3.35)
+
+subtotal = (l(6.00) or x(10.00)*t or t2 or t3 or t4) 
+tax = subtotal*0.13
+total = subtotal + tax
+print(total)
